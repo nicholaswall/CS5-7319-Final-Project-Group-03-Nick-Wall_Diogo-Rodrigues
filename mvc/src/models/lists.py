581820cae608f3100cc67e4ca4ccd33303f0434e
Nick Wall <60442663+nicholaswall@users.noreply.github.com>
2023-10-31 14:18:08 -0500

    feat: setup sqlite schema

---
+++ mvc/src/models/lists.py
@@ -0,0 +1,62 @@
+from mvc.src.db import con
+from dataclasses import dataclass
+from typing import List as ListType
+
+
+@dataclass
+class List:
+    id: int
+    name: str
+
+
+class ListsModel:
+    def __init__(self):
+        self.cursor = con.cursor()
+        self.table_name = "lists"
+
+        sql = (
+            "CREATE TABLE IF NOT EXISTS "
+            + self.table_name
+            + " (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)"
+        )
+        print("Creating table: ", sql)
+        self.cursor.execute(sql)
+
+    def create(self, name: str) -> List:
+        sql = (
+            "INSERT INTO "
+            + self.table_name
+            + " (name) VALUES ('"
+            + name
+            + "') RETURNING *"
+        )
+        created_list = self.cursor.execute(sql).fetchone()
+        con.commit()
+
+        return List(*created_list)
+
+    def get_all(self) -> ListType[List]:
+        sql = "SELECT * FROM " + self.table_name
+        lists = self.cursor.execute(sql).fetchall()
+        con.commit()
+
+        return [List(*list) for list in lists]
+
+    def get_by_id(self, id: int) -> List:
+        sql = "SELECT * FROM " + self.table_name + " WHERE id = " + str(id)
+        list = self.cursor.execute(sql).fetchone()
+        con.commit()
+
+        return List(*list)
+
+    def get_by_name(self, name: str) -> List:
+        sql = "SELECT * FROM " + self.table_name + " WHERE name = '" + name + "'"
+        list = self.cursor.execute(sql).fetchone()
+        con.commit()
+
+        return List(*list)
+
+    def delete(self, id: int) -> None:
+        sql = "DELETE FROM " + self.table_name + " WHERE id = " + str(id)
+        self.cursor.execute(sql)
+        con.commit()
---
+++ mvc/src/models/tasks.py
@@ -0,0 +1,80 @@
+from mvc.src.db import con, sql_value
+from dataclasses import dataclass
+from typing import List as ListType, Optional
+
+
+@dataclass
+class Task:
+    id: int
+    title: str
+    desciption: str
+    completed: bool
+    list_id: int
+    parent_id: int
+
+
+class TasksModel:
+    def __init__(self):
+        self.cursor = con.cursor()
+        self.table_name = "tasks"
+
+        sql = (
+            "CREATE TABLE IF NOT EXISTS "
+            + self.table_name
+            + """ (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                title TEXT NOT NULL, 
+                description TEXT,
+                completed BOOLEAN NOT NULL DEFAULT FALSE,
+                list_id INTEGER NOT NULL,
+                FOREIGN KEY(list_id) REFERENCES lists(id),
+                parent_id INTEGER,
+                FOREIGN KEY(parent_id) REFERENCES tasks(id)
+                   )
+            """
+        )
+        print("Creating table: ", sql)
+        self.cursor.execute(sql)
+
+    def create(
+        self, title: str, description: str, list_id: int, parent_id: Optional[int]
+    ) -> Task:
+        if not parent_id:
+            parent_id = "NULL"
+
+        sql = (
+            "INSERT INTO "
+            + self.table_name
+            + " (title, description, list_id, parent_id) VALUES "
+            + sql_value([title, description, list_id, parent_id])
+            + " RETURNING *"
+        )
+        created_task = self.cursor.execute(sql).fetchone()
+        con.commit()
+
+        return Task(*created_task)
+
+    def get_all(self) -> ListType[Task]:
+        sql = "SELECT * FROM " + self.table_name
+        tasks = self.cursor.execute(sql).fetchall()
+        con.commit()
+
+        return [Task(*task) for task in tasks]
+
+    def get_by_id(self, id: int) -> Task:
+        sql = "SELECT * FROM " + self.table_name + " WHERE id = " + str(id)
+        task = self.cursor.execute(sql).fetchone()
+        con.commit()
+
+        return Task(*task)
+
+    def get_by_title(self, title: str) -> Task:
+        sql = "SELECT * FROM " + self.table_name + " WHERE title = '" + title + "'"
+        task = self.cursor.execute(sql).fetchone()
+        con.commit()
+
+        return Task(*task)
+
+    def delete(self, id: int) -> None:
+        sql = "DELETE FROM " + self.table_name + " WHERE id = " + str(id)
+        self.cursor.execute(sql)
